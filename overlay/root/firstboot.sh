b62b2b3306944fd6417317fa0a6e3d3802c44d04
Dongjin Kim <tobetter@gmail.com>
2022-03-15 00:01:32 +0900

    firstboot: Restart SSH server at first boot
    Signed-off-by: Dongjin Kim <tobetter@gmail.com>
    Change-Id: I42f2b5468be4d1cd3faefb752f0f32f33776150c

--- overlay/root/firstboot.sh
+++ overlay/root/firstboot.sh
@@ -23,17 +23,6 @@ resize2fs ${rootdev}
 
 rm -f /etc/ssh/ssh_host* && ssh-keygen -A
 
-case $(cat /sys/firmware/devicetree/base/model) in
-        "Hardkernel Odroid XU4")
-                need_tweak="true"
-                ;;
-        *)
-                need_tweak="false"
-                ;;
-esac
-
-if [ "x${need_tweak}" = "xtrue" ]; then
-	service ssh restart
-fi
+service ssh restart
 
 exit 0
