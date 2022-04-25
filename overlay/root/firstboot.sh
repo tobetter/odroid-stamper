77eb28f70674f12a67682fbd472c2a8f6cbfb1c0
Dongjin Kim <tobetter@gmail.com>
2022-04-25 14:40:00 +0900

    firstboot: fix wrong calculation to extand rootfs partition
    Signed-off-by: Dongjin Kim <tobetter@gmail.com>
    Change-Id: I636d8b515b83876450d78aa5fc2ffc4273460e85

--- overlay/root/firstboot.sh
+++ overlay/root/firstboot.sh
@@ -14,7 +14,7 @@ case ${rootdev} in
 esac
 
 lba_start=`fdisk -l ${dev} | grep p2 | awk '{print $2}'`
-lba_finish=$((`fdisk -l ${dev} | grep Disk | grep sectors | awk '{printf $7}'` - 2048))
+lba_finish=$((`fdisk -l ${dev} | grep Disk | grep sectors | awk '{printf $7}'` - 1))
 
 echo -e "p\nd\n2\nn\np\n2\n${lba_start}\n${lba_finish}\np\nw\n" | \
 	fdisk ${dev} >/dev/null
