378ebcb5a78f3527bc94a971382af9a275bcc86a
Dongjin Kim <tobetter@gmail.com>
2022-07-20 14:11:10 +0900

    custom: Add new build target for RescueZilla
    Signed-off-by: Dongjin Kim <tobetter@gmail.com>
    Change-Id: If73f25ffb2eeeb6ce7f6228dc24a4545ea3b1bb1

---
+++ custom/rescuezilla/overlay/home/ubuntu/.config/openbox/autostart.sh
@@ -0,0 +1,13 @@
+# This shell script is run before Openbox launches.
+# Environment variables set here are passed to the Openbox session.
+
+export OOO_FORCE_DESKTOP="gnome"
+export WINDOW_MANAGER="/usr/bin/openbox"
+export DESKTOP_SESSION="gnome"
+export GNOME_DESKTOP_SESSION_ID="openbox"
+export DE="openbox"
+
+# D-bus
+if which dbus-launch >/dev/null && test -z "$DBUS_SESSION_BUS_ADDRESS"; then
+       eval `dbus-launch --sh-syntax --exit-with-session`
+fi
